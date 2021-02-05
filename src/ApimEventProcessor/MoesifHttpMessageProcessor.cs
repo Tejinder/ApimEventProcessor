4374785c65bc6424f1e89e680dccfcbd2caca9f0
Tejinder <tejinder456@gmail.com>
2021-02-05 17:22:32 +0530

    Update MoesifHttpMessageProcessor.cs

--- src/ApimEventProcessor/MoesifHttpMessageProcessor.cs
+++ src/ApimEventProcessor/MoesifHttpMessageProcessor.cs
@@ -218,7 +218,7 @@ namespace ApimEventProcessor
                 ApiVersion = _ApiVersion,
                 /*IpAddress = null,*/
 				/*IpAddress = (String)request.HttpRequestMessage.Headers.GetValues("clientIPAddress").FirstOrDefault(),*/
-				IpAddress = h.Properties["ipaddress"],
+				IpAddress = (String)h.Properties["ipaddress"],
                 Body = reqBodyWrapper.Item1,
                 TransferEncoding = reqBodyWrapper.Item2
             };
